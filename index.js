e25d94f6ccbc6ceee7cd516be794b39aaa943e58
abhishekmaniy <abhishekmaniyar502@gmail.com>
2025-05-23 10:36:14 +0530

    added test 2 for webhook

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hello From node demo 2");
 });
 
+app.get("/about", (req, res) => {
+    res.send("Hello From node demo 2 about page");
+});
+
 app.listen(3000, () => {
     console.log("app is Running on port 3000")
 })
